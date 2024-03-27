aa206d7d1144e86598052a9f7db061e5de0fc4b9
OlegFilippov1959 <164005331+OlegFilippov1959@users.noreply.github.com>
2024-03-27 17:48:16 +0300

    Update main.py
    Delete comments

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ def index():
     return '<h1> Hello World! </h1>'
 @app.route('/user/<name>')
 def user(name):
-    #name = name + add_info
     name2 = gitworkcheck.add_info(name)
     return '<h1> Hi hi, %s </h1>' % name2
-    #return '<h1> Hi hi, %s </h1>' % name
 if __name__ == '__main__':
     app.run(debug=True)
