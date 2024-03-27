af47223368540d5a109716a4b7d76dd8bf0b23c4
Filippov Oleg <ov-filippov@yandex.ru>
2024-03-27 17:06:03 +0300

    add gitworkcheck.py and edit main.py third commit

---
+++ gitworkcheck.py
@@ -0,0 +1,2 @@
+def add_info(name: str) -> str:
+    return name + ' the best man'
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import gitworkcheck
 app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1> Hello World! </h1>'
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> Hi hi, %s </h1>' % name
+    #name = name + add_info
+    name2 = gitworkcheck.add_info(name)
+    return '<h1> Hi hi, %s </h1>' % name2
+    #return '<h1> Hi hi, %s </h1>' % name
 if __name__ == '__main__':
     app.run(debug=True)
