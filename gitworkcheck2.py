b7101ccd2c17e93b7d7aacdf90e1f22577e1fb71
Filippov Oleg <ov-filippov@yandex.ru>
2024-03-28 17:52:54 +0300

    add gitworkcheck2.py and edit main.py fourth commit

---
+++ gitworkcheck2.py
@@ -0,0 +1,2 @@
+def add_info_firstbranch(name: str) -> str:
+    return name + ' you are best the whole world'
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import gitworkcheck
+import gitworkcheck2
 app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1> Hello World! </h1>'
 @app.route('/user/<name>')
 def user(name):
-    name2 = gitworkcheck.add_info(name)
+    name2 = gitworkcheck.add_info(name) + ' ' + gitworkcheck2.add_info_firstbranch(name)
     return '<h1> Hi hi, %s </h1>' % name2
 if __name__ == '__main__':
     app.run(debug=True)
